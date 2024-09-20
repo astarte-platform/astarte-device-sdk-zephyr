7b1e2c3f90ed17199c2551d517455fc7d201c005
Luca Arato <luca.arato@secomind.com>
2024-09-20 15:01:42 +0200

    Refactor to avoid using the BOARD cmake variable
    Signed-off-by: Luca Arato <luca.arato@secomind.com>

--- samples/common/Utils.cmake
+++ samples/common/Utils.cmake
@@ -4,11 +4,10 @@
 
 function(concat_if_exists FILE_PATH STRING_NAME)
     # Check if the file exists
-    file(GLOB FILE_TO_ADD ${FILE_PATH})
+    get_filename_component(FILE_TO_ADD ${FILE_PATH} ABSOLUTE)
     
     # If the file exists, add it to the list
     if(EXISTS ${FILE_TO_ADD})
         set(${STRING_NAME} "${${STRING_NAME}};${FILE_PATH}" PARENT_SCOPE)
     endif()
 endfunction()
-
