91770d4c2354704d775a0070dfea3cc2d7abed24
Simone Orru <simone.orru@secomind.com>
2026-02-25 12:09:37 +0100

    Remove static keyword
    Also rename namespace in nsp as reserved in C++
    
    Signed-off-by: Simone Orru <simone.orru@secomind.com>

--- include/astarte_device_sdk/device_id.h
+++ include/astarte_device_sdk/device_id.h
@@ -45,7 +45,7 @@ extern "C" {
  * @param[out] out A buffer where the result will be written.
  * @return ASTARTE_RESULT_OK if successful, otherwise an error code.
  */
-astarte_result_t astarte_device_id_generate_random(char out[static ASTARTE_DEVICE_ID_LEN + 1]);
+astarte_result_t astarte_device_id_generate_random(char out[ASTARTE_DEVICE_ID_LEN + 1]);
 
 /**
  * @brief Generate a deterministic Astarte device ID.
@@ -53,15 +53,15 @@ astarte_result_t astarte_device_id_generate_random(char out[static ASTARTE_DEVIC
  * @details The Astarte device ID will be generated using an UUIDv5. An UUIDv5 is created hasing
  * a namespace and name. The namespace is an UUID itself (of any version).
  *
- * @param[in] namespace The namespace used to generate the device ID.
+ * @param[in] nsp The namespace used to generate the device ID.
  * @param[in] name The name used to generate the device ID.
  * @param[in] name_size The size of the @p name buffer.
  * @param[out] out A buffer where the result will be written.
  * @return ASTARTE_RESULT_OK if successful, otherwise an error code.
  */
 astarte_result_t astarte_device_id_generate_deterministic(
-    const uint8_t namespace[static ASTARTE_DEVICE_ID_NAMESPACE_SIZE], const uint8_t *name,
-    size_t name_size, char out[static ASTARTE_DEVICE_ID_LEN + 1]);
+    const uint8_t nsp[ASTARTE_DEVICE_ID_NAMESPACE_SIZE], const uint8_t *name, size_t name_size,
+    char out[ASTARTE_DEVICE_ID_LEN + 1]);
 
 #ifdef __cplusplus
 }
